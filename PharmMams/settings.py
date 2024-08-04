9adf8e396636db53bdd5576910b74f6059c82986
unknown <favourelodimuor15@gmail.com>
2024-08-04 16:44:38 +0100

    To be deployed

--- PharmMams/settings.py
+++ PharmMams/settings.py
@@ -51,8 +51,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["mamspharm.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
